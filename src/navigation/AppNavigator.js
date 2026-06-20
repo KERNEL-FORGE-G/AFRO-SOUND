a7d74e2bed1d70b4e6658f80ea14cce82e429566
Archlord12345 <ravel.nghomsi@facsciences-uy1.cm>
2026-06-20 19:04:09 +0100

    fix: separate Bibliothèque and Téléchargement pages

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -50,6 +50,8 @@ const TabIcon = ({focused, color, size, routeName}) => {
     iconName = focused ? 'add-circle' : 'add-circle-outline';
   } else if (routeName === 'Hors-ligne') {
     iconName = focused ? 'cloud-offline' : 'cloud-offline-outline';
+  } else if (routeName === 'Téléchargement') {
+    iconName = focused ? 'cloud-download' : 'cloud-download-outline';
   } else if (routeName === 'Bibliothèque') {
     iconName = focused ? 'library' : 'library-outline';
   } else if (routeName === 'Profil') {
@@ -91,7 +93,8 @@ function MainTabs() {
         <Tab.Screen name="Accueil" component={Home} />
         <Tab.Screen name="Rechercher" component={Search} />
         <Tab.Screen name="Créer" component={CreatePlaylist} />
-        <Tab.Screen name="Bibliothèque" component={OfflineLibrary} />
+        <Tab.Screen name="Bibliothèque" component={Library} />
+        <Tab.Screen name="Téléchargement" component={OfflineLibrary} />
         <Tab.Screen name="Profil" component={Profile} />
       </Tab.Navigator>
 
